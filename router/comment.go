1ba60bd5c01dc9943a293f4fb8bb47af4d91e51f
lxuegod <lxuegod@163.com>
2022-09-21 19:06:33 +0800

    修改评论路由

--- router/comment.go
+++ router/comment.go
@@ -4,11 +4,9 @@ import "github.com/MiniDouyin/controller"
 
 func CommentRouter() {
 	// GET
-	DouyinRouter.GET("/comment/comment/", controller.GetComment)
-	DouyinRouter.GET("/comment/count/", controller.GetCommentFavoriteCount)
-	DouyinRouter.GET("/comment/front_list/", controller.GetCommentListFront)
-	DouyinRouter.GET("/comment/list", controller.GetCommentList)
+	DouyinRouter.GET("/comment/list", controller.CommitList)
+
 	// POST
-	DouyinRouter.POST("/comment/delete/", controller.DeleteComment)
-	DouyinRouter.POST("/comment/action/", controller.AddComment)
+	DouyinRouter.POST("/comment/action", controller.CommentAction)
+
 }
